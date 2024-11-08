e6e2bd53b699bf9538210a681d9c46e271ccfedc
turtle <alessio.tartufoli@studenti.unicam.it>
2024-11-08 19:21:36 +0100

    Initial commit

---
+++ backend/src/main/java/it/unicam/progettoWebApp24/ProgettoWebApp24Application.java
@@ -0,0 +1,13 @@
+package it.unicam.progettoWebApp24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgettoWebApp24Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgettoWebApp24Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/it/unicam/progettoWebApp24/ProgettoWebApp24ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unicam.progettoWebApp24;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgettoWebApp24ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
